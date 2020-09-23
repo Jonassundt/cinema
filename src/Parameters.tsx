6972058e019c31fd179942fdeb10bda8065b8524
Jonas Sæther <jonas.sath@outlook.com>
2020-09-23 22:07:48 +0200

    wrapper called posterElement for connecting audio, animation to poster

--- src/Parameters.tsx
+++ src/Parameters.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const initialState = {
-    light: 30,
-    volume: 0,
-    animation: true,
-    color: true,
-    fullscreen: false,
-    slideshow: false,
-    posterIndex: 0
-}
+  light: 30,
+  volume: 0,
+  animation: true,
+  color: true,
+  fullscreen: false,
+  slideshow: false,
+  posterIndex: 0,
+};
 
 const Parameters = React.createContext({
-    params: initialState, setParams: (params: any) => {
-    }
+  params: initialState,
+  setParams: (params: any) => {},
 });
 
 export default Parameters;
---
+++ src/components/PosterElement/PosterElement.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import "./posterElements.css";
+
+import Parameters from "../../Parameters";
+
+interface PosterElementProps {
+  poster: any;
+  height: number;
+}
+
+type PosterElementState = {
+  playing: boolean;
+  audio: any;
+  audioIndex: number;
+};
+
+class PosterElement extends React.Component<
+  PosterElementProps,
+  PosterElementState
+> {
+  constructor(props: PosterElementProps) {
+    super(props);
+    this.state = { playing: false, audio: null, audioIndex: -1 };
+  }
+
+  static contextType = Parameters;
+
+  playAudio = () => {
+    if (!this.state.playing) {
+      this.state.audio.load();
+      this.state.audio.play();
+      this.state.audio.volume = this.context.params.volume / 10;
+      this.setState({ playing: true });
+    } else {
+      this.state.audio.volume = this.context.params.volume / 10;
+    }
+  };
+
+  componentDidMount() {
+    const audio = new Audio(`/sounds/${this.props.poster.props.name}.mp3`);
+    this.setState({ audio, audioIndex: this.context.params.posterIndex });
+    if (this.context.params.volume) {
+      this.playAudio();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.audioIndex === this.context.params.posterIndex) {
+      if (this.context.params.volume) {
+        this.playAudio();
+      } else if (this.state.playing) {
+        this.state.audio.pause();
+        this.setState({ playing: false });
+      }
+    } else {
+      this.state.audio.pause();
+      const audio = new Audio(`/sounds/${this.props.poster.props.name}.mp3`);
+      this.setState({
+        playing: false,
+        audio,
+        audioIndex: this.context.params.posterIndex,
+      });
+      this.playAudio();
+    }
+  }
+
+  render() {
+    const animationName = this.props.poster.props.name + "Animation";
+
+    const props = {
+      height: this.props.height,
+      width: this.props.height / 1.333,
+      className: this.context.params.animation ? animationName : "",
+    };
+    return React.cloneElement(this.props.poster, props);
+  }
+}
+
+export default PosterElement;
