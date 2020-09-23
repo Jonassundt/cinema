cbe4fa0e159ebbec5f07d94d0958980976260442
Jonas Sæther <jonas.sath@outlook.com>
2020-09-23 22:34:17 +0200

    mapped posters to text-fetching

--- src/components/Poster/Poster.tsx
+++ src/components/Poster/Poster.tsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import styles from "./poster.module.css";
 
 import PosterElement from "../PosterElement/PosterElement";
+import FetchMovie from "./fetchData";
 
 import posterImages from "../Images";
 
@@ -45,29 +46,11 @@ function Poster() {
     return;
   };
 
-    return (
-        <div>
-            <div className={params.fullscreen ? styles.PosterFullscreen : styles.Poster}>
-                <img
-                    className={params.posterIndex > 0 ? styles.ArrowButtons : styles.ArrowGrayed}
-                    src="icons/leftarrow.svg"
-                    alt="left arrow"
-                    onClick={prevPoster}
-                />
-                <div className={leftAnimation ? styles.LeftAnimation : rightAnimation ? styles.RightAnimation : styles.Frame} style={{ filter: `grayscale(${params.color ? 0 : 1})` }}>
-                    {posters[params.posterIndex]}
-                </div>
-                <img
-                    className={params.posterIndex < posters.length - 1 ? styles.ArrowButtons : styles.ArrowGrayed}
-                    src="icons/rightarrow.svg"
-                    alt="right arrow"
-                    onClick={nextPoster}
-                />
-            </div>
-            <div className={params.fullscreen ? styles.DescriptionFullscreen : styles.Description}>
-                {leftAnimation || rightAnimation ? null : <div className={styles.DescriptionText} > 
-                <FetchMovie></FetchMovie>
-                 </div>}    </div></div>
+  useEffect(() => {
+    document.addEventListener(
+      "keydown",
+      (event) => keyboardEvent(event.keyCode),
+      false
     );
   });
 
@@ -114,19 +97,7 @@ function Poster() {
       >
         {leftAnimation || rightAnimation ? null : (
           <div className={styles.DescriptionText}>
-            {" "}
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-            of Good and Evil) by Cicero, written in 45 BC. This book is a
-            treatise on the theory of ethics, very popular during the
-            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-            amet..", comes from a line in section 1.10.32.
+            <FetchMovie></FetchMovie>
           </div>
         )}{" "}
       </div>
--- src/components/Poster/fetchData.tsx
+++ src/components/Poster/fetchData.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import styles from "./fetchData.module.css";
 
+import Parameters from "../../Parameters";
+
+//bond-skyfall, inception, interstellar, batman, shawshank, starwars
+const movieIds = [37724, 27205, 157336, 272, 278, 1891];
+
 export default class fetchMovie extends React.Component {
+  static contextType = Parameters;
+
   state = {
     loading: true,
-    movie: {title: "", tagline: "", overview: ""},
-    ID: [],
-    apiKey: "4d785ede4f71452009daf3f954680323"
+    movie: { title: "", tagline: "", overview: "" },
+    movieIds,
+    apiKey: "4d785ede4f71452009daf3f954680323",
   };
 
-  /* Shawshank ID : 278
-  *  Batman begins ID : 272
-  *  Skyfall ID : 37724
-  *  Inception ID : 27205
-  *  Interstellar ID : 157336
-  *  Star Wars ID : 1891
-  * 
-  */
-
   async componentDidMount() {
-    const url =
-      `https://api.themoviedb.org/3/movie/27205?api_key=${this.state.apiKey}`;
+    const url = `https://api.themoviedb.org/3/movie/${
+      movieIds[this.context.params.posterIndex]
+    }?api_key=${this.state.apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     this.setState({ movie: data, loading: false });
   }
-
-
 
   render() {
     return (
@@ -36,20 +33,12 @@ export default class fetchMovie extends React.Component {
           <div>Loading...</div>
         ) : (
           <div className={styles.container}>
-              <div className={styles.tagline}>"{this.state.movie.tagline}"</div>
+            <div className={styles.tagline}>"{this.state.movie.tagline}"</div>
             <div className={styles.title}>{this.state.movie.title}</div>
-        <div className={styles.overview}>{this.state.movie.overview}</div>
-          
+            <div className={styles.overview}>{this.state.movie.overview}</div>
           </div>
         )}
       </div>
     );
   }
 }
-
-/**
- * API Key (v3 auth) = 4d785ede4f71452009daf3f954680323
- * Example API request = https://api.themoviedb.org/3/movie/550?api_key=4d785ede4f71452009daf3f954680323
- * API read access token (v4 auth) = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZDc4NWVkZTRmNzE0NTIwMDlkYWYzZjk1NDY4MDMyMyIsInN1YiI6IjVmNmI0ZDVlZDU1ZTRkMDAzNTZkOGU3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cOWe7DI7D4JOZdIaZWnhu-8hddvqkySPZkAF6VjFvOo
- * interstellar : https://api.themoviedb.org/3/movie/157336?api_key=4d785ede4f71452009daf3f954680323
- */
