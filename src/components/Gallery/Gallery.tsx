763b41f68a81f0592fb331d9dacbe2c6f89f5344
Jonas Sæther <jonas.sath@outlook.com>
2020-09-27 14:49:12 +0200

    fixed bug on favorites

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,15 +1,10 @@
 import React, { useContext } from "react";
 
 import Parameters from "../../Parameters";
-import posters from "../Images";
 
 import posterImages from "../Images";
 
 import styles from "./gallery.module.css";
-
-interface indexes {
-  [key: string]: any;
-}
 
 function Gallery() {
   const { params, setParams } = useContext(Parameters);
@@ -26,14 +21,15 @@ function Gallery() {
 
   return (
     <div className={styles.Gallery}>
-      {posterImages.filter((poster, index) => params.favorites.includes(index) || !params.showFavorites).map((poster, index) => (
-        <div
-          className={styles.Thumbnail}
-          key={index}
-          onClick={() => setPosterIndex(index)}
-        >
-          <div className={styles.SmallPoster}>{poster}</div>
-        </div>
+      {posterImages.map((poster, index) => (
+        (params.favorites.includes(index) || !params.showFavorites) ?
+          <div
+            className={styles.Thumbnail}
+            key={index}
+            onClick={() => setPosterIndex(index)}
+          >
+            <div className={styles.SmallPoster}>{poster}</div>
+          </div> : null
       ))}
     </div>
   );
