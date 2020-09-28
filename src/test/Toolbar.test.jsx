c285d09433d0e0a327e69bc9a155bba5f2293b04
Jonas Sæther <jonas.sath@outlook.com>
2020-09-28 20:06:40 +0200

    #7 jest test

--- src/test/Toolbar.test.jsx
+++ src/test/Toolbar.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Toolbar from '../components/Toolbar/Toolbar.tsx';
 
-describe('testing the toolbar', () => {
+describe('testing the toolbar ', () => {
     it("matches the snapshot", () => {
         const tree = renderer.create(<Toolbar />).toJSON();
 
