bd6860dd6b73466053858226800b17c3579b0ef0
Jonas Sundt <jonassundt98@gmail.com>
2023-05-07 16:14:19 +0200

    Deployed ReactApp to Github pages

---
+++ precache-manifest.50a625feec94b43681384ef6a0c7ff1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd7a913cd36dac618115ca3a22edf96",
+    "url": "/cinema/index.html"
+  },
+  {
+    "revision": "f82573526a8b25e72e2b",
+    "url": "/cinema/static/css/main.1be9a15c.chunk.css"
+  },
+  {
+    "revision": "74b6de6287335d6c46a7",
+    "url": "/cinema/static/js/2.f0eca474.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cinema/static/js/2.f0eca474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82573526a8b25e72e2b",
+    "url": "/cinema/static/js/main.a99e60ba.chunk.js"
+  },
+  {
+    "revision": "c2bbc24c0d20a2031696",
+    "url": "/cinema/static/js/runtime-main.31808535.js"
+  },
+  {
+    "revision": "c4e8b5c442e6143c1b8beeb8dfd2c31b",
+    "url": "/cinema/static/media/batman.c4e8b5c4.svg"
+  },
+  {
+    "revision": "1b65982a521ed33b43dc2926e03fbdc5",
+    "url": "/cinema/static/media/inception.1b65982a.svg"
+  },
+  {
+    "revision": "b31a791bdfa3f6a4871b6a3d04d455f0",
+    "url": "/cinema/static/media/interstellar.b31a791b.svg"
+  },
+  {
+    "revision": "672e4dbd5ba3de0093c1924faa7c53f0",
+    "url": "/cinema/static/media/jamesbond.672e4dbd.svg"
+  },
+  {
+    "revision": "5e06b7ffdc473cf787aa4fc18153823d",
+    "url": "/cinema/static/media/shawshank.5e06b7ff.svg"
+  },
+  {
+    "revision": "48213ee317dfd798c7f10a71bfee3554",
+    "url": "/cinema/static/media/starwars.48213ee3.svg"
+  }
+]);
